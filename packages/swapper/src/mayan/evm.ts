40366a89d5dfc356ec2ef6f8605318d2fc72c7e2
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-04-02 14:23:47 +0900

    Update evm.ts

--- packages/swapper/src/mayan/evm.ts
+++ packages/swapper/src/mayan/evm.ts
@@ -3,7 +3,7 @@ import { Quote as MayanQuote, getSwapFromEvmTxPayload } from "@mayanfinance/swap
 
 export function buildEvmQuoteData(request: QuoteRequest, routeData: MayanQuote): QuoteData {
     const signerChainId = routeData.fromToken.chainId;
-    const swapData = getSwapFromEvmTxPayload(routeData, request.from_address, request.to_address, { evm: request.referral_address }, request.from_address, signerChainId, null, null);
+    const swapData = getSwapFromEvmTxPayload(routeData, request.from_address, request.to_address, { solana: request.referral_address }, request.from_address, signerChainId, null, null);
     const value = BigInt(swapData.value || 0);
     return {
         to: swapData.to?.toString() || "",
