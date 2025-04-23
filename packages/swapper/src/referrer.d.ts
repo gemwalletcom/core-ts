04f5f005ee56a89d3fc5b3fbe9c7e44029d0d3d5
0xh3rman <119309671+0xh3rman@users.noreply.github.com>
2025-04-23 10:35:56 +0900

    remove map files

--- packages/swapper/src/referrer.d.ts
+++
@@ -1,9 +0,0 @@
-export type Referrers = {
-    evm: string;
-    solana: string;
-    sui: string;
-    ton: string;
-    tron: string;
-};
-export declare function getReferrerAddresses(): Referrers;
-//# sourceMappingURL=referrer.d.ts.map
